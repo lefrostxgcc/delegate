8aee1b73e0620b17c20c92405e1f8f5b79d47814
Chip <chipz68q@gmail.com>
2018-12-18 22:56:45 +0500

    Add delegate param to dice create

--- include/dice.h
+++ include/dice.h
@@ -1,9 +1,11 @@
 #ifndef CH_DICE_H
 #define CH_DICE_H
 
+#include <delegate.h>
+
 struct ch_dice;
 
-struct ch_dice*		dice_create(void (*show_delegate)(int, int));
+struct ch_dice*		dice_create(struct ch_dice_delegate *delegate);
 void				dice_go(struct ch_dice *dice);
 struct ch_dice*		dice_destroy(struct ch_dice *dice);
 
--- src/dice.c
+++ src/dice.c
@@ -6,20 +6,20 @@ enum {DICE_MIN = 1, DICE_MAX = 6, DICE_RANGE = DICE_MAX - DICE_MIN};
 
 struct ch_dice
 {
-	void (*show)(int, int);
-	int a;
-	int b;
+	struct ch_dice_delegate		*delegate;
+	int							a;
+	int							b;
 };
 
 static int			random_dice(void);
 
-struct ch_dice*		dice_create(void (*show_delegate)(int, int))
+struct ch_dice*		dice_create(struct ch_dice_delegate *delegate)
 {
 	struct ch_dice	*dice;
 
 	srand(time(NULL));
 	dice = (struct ch_dice *) malloc(sizeof(struct ch_dice));
-	dice->show = show_delegate;
+	dice->delegate = delegate;
 	return dice;
 }
 
@@ -27,7 +27,7 @@ void				dice_go(struct ch_dice *dice)
 {
 	dice->a = random_dice();
 	dice->b = random_dice();
-	dice->show(dice->a, dice->b);
+	dice_delegate_go(dice->delegate, dice->a, dice->b);
 }
 
 struct ch_dice*		dice_destroy(struct ch_dice *dice)
